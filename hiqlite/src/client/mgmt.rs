11fa67f4bf50b3bd9505828ba5719477b1723b4a
sebadob <sebastiandobe@mailbox.org>
2024-10-28 08:06:43 +0100

    make it possible to restore a backup from a local SQLite file

--- hiqlite/src/client/mgmt.rs
+++ hiqlite/src/client/mgmt.rs
@@ -208,6 +208,7 @@ impl Client {
 
         // We need to do a short sleep only to avoid race conditions during rolling releases.
         // This also helps to make re-joins after a restart smoother.
+        // TODO for some very weird reason, the process gets stuck during this sleep await when testing
         time::sleep(Duration::from_secs(5)).await;
 
         info!("Shutdown complete");
