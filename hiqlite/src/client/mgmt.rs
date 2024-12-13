e7e6ca968f0f39ee33f5adac95afda14837bc7a3
sebadob <sebastiandobe@mailbox.org>
2024-12-13 16:29:19 +0100

    revert shutdown shortening as it does not work well in production

--- hiqlite/src/client/mgmt.rs
+++ hiqlite/src/client/mgmt.rs
@@ -196,52 +196,6 @@ impl Client {
         #[cfg(feature = "sqlite")] tx_client_db: &flume::Sender<ClientStreamReq>,
         tx_shutdown: &Option<watch::Sender<bool>>,
     ) -> Result<(), Error> {
-        // Before initiating the shutdown, we want to add a small delay for 2 reasons:
-        // - smooth out k8s rolling releases
-        // - with openraft 0.10, trigger a leader election before the delay to make it even smoother
-
-        let mut election_timeout_max = 1500;
-        #[cfg(feature = "cache")]
-        {
-            election_timeout_max = max(
-                election_timeout_max,
-                state.raft_cache.raft.config().heartbeat_interval,
-            );
-        }
-        #[cfg(feature = "sqlite")]
-        {
-            election_timeout_max = max(
-                election_timeout_max,
-                state.raft_db.raft.config().election_timeout_max,
-            );
-        }
-
-        // TODO as soon as openraft 0.10 is stable, trigger a graceful leader change here
-
-        /*
-        TODO this is very weird with openraft 0.9 -> something seems to be blocking internally
-        Only during integration tests, we can mess up the internal raft, if we sleep for
-        >= heartbeat_interval here. This has to do something with the way how tokio tests use
-        timing internally + the fact that something inside openraft 0.9 must be blocking, as I do
-        not have any other explanation for this. The upfront sleep is totally fine in any other
-        scenario. Anything else does not make sense, as just an async sleep should never mess up
-        logic in another place.
-        Currently, when we shut down a single client during tests and restart after deleting the
-        state machine and receive a snapshot and so on, the raft can get stuck. But again, only
-        inside #[tokio::test] when 3 nodes are started from the same test runtime
-         */
-
-        #[cfg(feature = "cache")]
-        {
-            info!("Shutting down raft cache layer");
-            match state.raft_cache.raft.shutdown().await {
-                Ok(_) => {}
-                Err(err) => {
-                    return Err(Error::Error(err.to_string().into()));
-                }
-            }
-        }
-
         #[cfg(feature = "sqlite")]
         {
             info!("Shutting down raft sqlite layer");
@@ -295,11 +249,8 @@ impl Client {
         // Note: The issue is "something blocking" in `openraft` but only in some conditions that
         // don't make sense to me yet - needs further investigation until this sleep can be removed
         // safely.
-        let delay = election_timeout_max * 5;
-        info!("Shutting down in {} ms ...", delay);
-        time::sleep(Duration::from_millis(delay)).await;
-        // info!("Shutting down in 10 s ...");
-        // time::sleep(Duration::from_secs(10)).await;
+        info!("Shutting down in 10 s ...");
+        time::sleep(Duration::from_secs(10)).await;
 
         info!("Shutdown complete");
         Ok(())
