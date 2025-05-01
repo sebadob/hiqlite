0c1dbce95eaaaef08a96470d99de70ab20c6ba70
sebadob <sebastiandobe@mailbox.org>
2025-05-01 16:36:54 +0200

    create `hiqlite-macros` crate and impl `params!()` + re-export `Embed` properly

---
+++ hiqlite-macros/src/lib.rs
@@ -0,0 +1,19 @@
+pub mod embed {
+    pub use rust_embed::{self, *};
+}
+
+/// Helper macro to created Owned Params which can be serialized and sent
+/// over the Raft network between nodes.
+#[macro_export]
+macro_rules! params {
+    ( $( $param:expr ),* ) => {
+        {
+            #[allow(unused_mut)]
+            let mut params = Vec::with_capacity(2);
+            $(
+                params.push(hiqlite::Param::from($param));
+            )*
+            params
+        }
+    };
+}
