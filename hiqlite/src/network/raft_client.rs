c7dfd1e301279e28fbd9a18c56c5784413a0b577
sebadob <sebastiandobe@mailbox.org>
2025-12-08 12:05:44 +0100

    more logs + switch places for `select!()` in WebSocket handler

--- hiqlite/src/network/raft_client.rs
+++ hiqlite/src/network/raft_client.rs
@@ -20,7 +20,7 @@ use tokio::io::{ReadHalf, WriteHalf};
 use tokio::sync::oneshot;
 use tokio::task::JoinHandle;
 use tokio::{select, task, time};
-use tracing::{debug, error};
+use tracing::{debug, error, info, warn};
 
 #[cfg(feature = "cache")]
 use crate::store::state_machine::memory::TypeConfigKV;
@@ -195,16 +195,16 @@ impl NetworkStreaming {
         'outer: loop {
             if is_raft_stopped.load(Ordering::Relaxed) {
                 if !is_startup_finished.load(Ordering::Relaxed) {
-                    debug!("Raft is still starting up - skipping initial connection");
+                    info!("Raft is still starting up - skipping initial connection");
                     time::sleep(Duration::from_secs(1)).await;
                     continue;
                 }
 
-                debug!("Raft is stopped - exiting NetworkStreaming::ws_handler()");
+                info!("Raft is stopped - exiting NetworkStreaming::ws_handler()");
                 break;
             }
 
-            debug!("Trying to open WebSocket stream");
+            info!("Trying to open WebSocket stream");
             let socket = {
                 match web_socket_connect::try_connect(
                     this_node,
@@ -216,7 +216,7 @@ impl NetworkStreaming {
                 .await
                 {
                     Ok(socket) => {
-                        debug!("WebSocket connected successfully");
+                        info!("WebSocket connected successfully");
                         socket
                     }
                     Err(err) => {
@@ -225,6 +225,8 @@ impl NetworkStreaming {
                         for _ in 0..3 {
                             // if there is a network error, no reason to try too hard to connect
                             time::sleep(Duration::from_millis(heartbeat_interval)).await;
+
+                            info!("Draining piled up logs: {}", rx.len());
 
                             // make sure channel is always free
                             if let Ok(req) = rx.try_recv() {
@@ -253,6 +255,8 @@ impl NetworkStreaming {
                             }
                         }
 
+                        info!("Initiating re-connect");
+
                         continue;
                     }
                 }
@@ -274,9 +278,10 @@ impl NetworkStreaming {
             let handle_write = task::spawn(Self::stream_writer(write, rx_write));
 
             loop {
+                // TODO include a timeout of heartbeat interval to re-connect?
                 let res = select! {
-                    res = rx.recv_async() => res,
                     res = rx_read.recv_async() => res,
+                    res = rx.recv_async() => res,
                 };
 
                 let req = match res {
@@ -379,10 +384,13 @@ impl NetworkStreaming {
         mut read: FragmentCollectorRead<ReadHalf<TokioIo<Upgraded>>>,
         tx: flume::Sender<RaftRequest>,
     ) {
+        // TODO we could maybe wrap this into a timeout to re-connect if no messages
+        //  came in after heartbeat interval * 3 or something like that
+
         while let Ok(frame) = read
             .read_frame(&mut |frame| async move {
                 // TODO obligated sends should be auto ping / pong / close ? -> verify!
-                debug!(
+                warn!(
                     "Received obligated send in stream client: OpCode: {:?}: {:?}",
                     frame.opcode.clone(),
                     frame.payload
@@ -410,7 +418,7 @@ impl NetworkStreaming {
             }
         }
 
-        debug!("Exiting Client Stream Reader");
+        warn!("Exiting Client Stream Reader");
     }
 
     async fn stream_writer(
@@ -470,6 +478,7 @@ impl NetworkConnectionStreaming {
             self.node.addr_raft
         );
 
+        // TODO does it make sense to wrap in a timeout as well?
         self.sender.send_async(req).await.map_err(|err| {
             error!(
                 "NetworkConnectionStreaming::send to node {}: {}",
