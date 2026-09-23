788102436ccd77286b9898a0b62d783bd8f4b280
sebadob <sebastiandobe@mailbox.org>
2026-09-23 10:48:34 +0200

    add `Client::try_migrate()`

--- hiqlite/src/client/migrate.rs
+++ hiqlite/src/client/migrate.rs
@@ -11,10 +11,13 @@ use tracing::{info, warn};
 impl Client {
     /// Execute database migrations.
     ///
-    /// To make this work, you currently need to add `rust-embed` to your `Cargo.toml`.
-    /// Then embed the Migrations into your binary to be able to execute them in a typüe-safe way:
+    /// To make this work, you currently need to `Embed` a `struct` in your app to be able to
+    /// execute them in a type-safe way:
+    ///
     /// ```rust, notest
-    /// #[derive(rust_embed::Embed)]
+    /// use hiqlite::macros::embed::*;
+    ///
+    /// #[derive(Embed)]
     /// #[folder = "migrations"]
     /// struct Migrations;
     ///
@@ -23,13 +26,48 @@ impl Client {
     ///
     /// You might want to take a look at the
     /// [sqlite-only](https://github.com/sebadob/hiqlite/tree/main/examples/sqlite-only) example.
-    #[cold]
+    ///
+    /// # Panics
+    ///
+    /// If the given Migrations have an invalid format.
     pub async fn migrate<T: RustEmbed>(&self) -> Result<(), Error> {
+        let migrations = Migrations::build::<T>();
+        self.migrate_execute_dedup::<T>(migrations).await
+    }
+
+    /// Execute database migrations.
+    ///
+    /// This version throws an error instead of a `panic` on invalid migrations.
+    ///
+    /// To make this work, you currently need to `Embed` a `struct` in your app to be able to
+    /// execute them in a type-safe way:
+    ///
+    /// ```rust, notest
+    /// use hiqlite::macros::embed::*;
+    ///
+    /// #[derive(Embed)]
+    /// #[folder = "migrations"]
+    /// struct Migrations;
+    ///
+    /// client.migrate::<Migrations>().await?;
+    /// ```
+    ///
+    /// You might want to take a look at the
+    /// [sqlite-only](https://github.com/sebadob/hiqlite/tree/main/examples/sqlite-only) example.
+    pub async fn try_migrate<T: RustEmbed>(&self) -> Result<(), Error> {
+        let migrations = Migrations::try_build::<T>().map_err(Error::Sqlite)?;
+        self.migrate_execute_dedup::<T>(migrations).await
+    }
+
+    #[cold]
+    pub(crate) async fn migrate_execute_dedup<T: RustEmbed>(
+        &self,
+        mut migrations: Vec<Migration>,
+    ) -> Result<(), Error> {
         let applied: Vec<AppliedMigration> = self
             .query_map("SELECT * FROM _migrations ORDER BY id ASC", Params::new())
             .await
             .unwrap_or_default();
-        let mut migrations = Migrations::build::<T>();
 
         // At least the beginning of the just built and already applied migrations must match.
         // We can skip already existing ones early, so they are not sent through the Raft each
@@ -78,7 +116,9 @@ impl Client {
                     .was_leader_update_error(&err, &self.inner.leader_db, &self.inner.tx_client_db)
                     .await
                 {
-                    self.migrate_execute(Migrations::build::<T>()).await
+                    // cannot fail if validation was fine before
+                    self.migrate_execute(Migrations::try_build::<T>().unwrap())
+                        .await
                 } else {
                     Err(err)
                 }
@@ -86,7 +126,6 @@ impl Client {
         }
     }
 
-    #[cold]
     pub(crate) async fn migrate_execute(&self, migrations: Vec<Migration>) -> Result<(), Error> {
         if let Some(state) = self.is_leader_db_with_state().await {
             let res =
